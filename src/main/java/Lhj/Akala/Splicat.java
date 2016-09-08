11ab307be5bacd4496c023975f7d46d4f78ef8bc
wlgq7041 <742756107@qq.com>
2016-09-08 11:44:19 +0800

    新增一个

---
+++ src/main/java/Lhj/Akala/Splicat.java
@@ -0,0 +1,193 @@
+package Lhj.Akala;
+
+public class Splicat {
+	private Integer ARTICLE_ID;
+	private String TITLE;
+	private String SHOW_TITLE;
+	private String BRIEF;
+	private String URL;
+	private String KEYWORD;
+	private String REL_KEYWORD;
+	private String AUTHOR;
+	private String SOURCE;
+	private Integer IS_PICTURE;
+	private String PICTURE_URL;
+	private Integer IS_HOT;
+	private Integer IS_NEW;
+	private Integer IS_HEAD;
+	private String PUBLISH_BY;
+	private String PUBLISH_DATE;
+	private Integer CATALOG_ID;
+	private String SITENO;
+	private Integer TYPE;
+	private String LINK_URL;
+	private Integer STATE;
+	private Integer ORDERLINE;
+	private Integer REVIEW;
+	private Integer COMMENT_TIMES;
+	private Integer HITS;
+	private Integer IN_FLAG;
+	private String USER_RIGHT;
+	public Integer getARTICLE_ID() {
+		return ARTICLE_ID;
+	}
+	public void setARTICLE_ID(Integer aRTICLE_ID) {
+		ARTICLE_ID = aRTICLE_ID;
+	}
+	public String getTITLE() {
+		return TITLE;
+	}
+	public void setTITLE(String tITLE) {
+		TITLE = tITLE;
+	}
+	public String getSHOW_TITLE() {
+		return SHOW_TITLE;
+	}
+	public void setSHOW_TITLE(String sHOW_TITLE) {
+		SHOW_TITLE = sHOW_TITLE;
+	}
+	public String getBRIEF() {
+		return BRIEF;
+	}
+	public void setBRIEF(String bRIEF) {
+		BRIEF = bRIEF;
+	}
+	public String getURL() {
+		return URL;
+	}
+	public void setURL(String uRL) {
+		URL = uRL;
+	}
+	public String getKEYWORD() {
+		return KEYWORD;
+	}
+	public void setKEYWORD(String kEYWORD) {
+		KEYWORD = kEYWORD;
+	}
+	public String getREL_KEYWORD() {
+		return REL_KEYWORD;
+	}
+	public void setREL_KEYWORD(String rEL_KEYWORD) {
+		REL_KEYWORD = rEL_KEYWORD;
+	}
+	public String getAUTHOR() {
+		return AUTHOR;
+	}
+	public void setAUTHOR(String aUTHOR) {
+		AUTHOR = aUTHOR;
+	}
+	public String getSOURCE() {
+		return SOURCE;
+	}
+	public void setSOURCE(String sOURCE) {
+		SOURCE = sOURCE;
+	}
+	public Integer getIS_PICTURE() {
+		return IS_PICTURE;
+	}
+	public void setIS_PICTURE(Integer iS_PICTURE) {
+		IS_PICTURE = iS_PICTURE;
+	}
+	public String getPICTURE_URL() {
+		return PICTURE_URL;
+	}
+	public void setPICTURE_URL(String pICTURE_URL) {
+		PICTURE_URL = pICTURE_URL;
+	}
+	public Integer getIS_HOT() {
+		return IS_HOT;
+	}
+	public void setIS_HOT(Integer iS_HOT) {
+		IS_HOT = iS_HOT;
+	}
+	public Integer getIS_NEW() {
+		return IS_NEW;
+	}
+	public void setIS_NEW(Integer iS_NEW) {
+		IS_NEW = iS_NEW;
+	}
+	public Integer getIS_HEAD() {
+		return IS_HEAD;
+	}
+	public void setIS_HEAD(Integer iS_HEAD) {
+		IS_HEAD = iS_HEAD;
+	}
+	public String getPUBLISH_BY() {
+		return PUBLISH_BY;
+	}
+	public void setPUBLISH_BY(String pUBLISH_BY) {
+		PUBLISH_BY = pUBLISH_BY;
+	}
+	public String getPUBLISH_DATE() {
+		return PUBLISH_DATE;
+	}
+	public void setPUBLISH_DATE(String pUBLISH_DATE) {
+		PUBLISH_DATE = pUBLISH_DATE;
+	}
+	public Integer getCATALOG_ID() {
+		return CATALOG_ID;
+	}
+	public void setCATALOG_ID(Integer cATALOG_ID) {
+		CATALOG_ID = cATALOG_ID;
+	}
+	public String getSITENO() {
+		return SITENO;
+	}
+	public void setSITENO(String sITENO) {
+		SITENO = sITENO;
+	}
+	public Integer getTYPE() {
+		return TYPE;
+	}
+	public void setTYPE(Integer tYPE) {
+		TYPE = tYPE;
+	}
+	public String getLINK_URL() {
+		return LINK_URL;
+	}
+	public void setLINK_URL(String lINK_URL) {
+		LINK_URL = lINK_URL;
+	}
+	public Integer getSTATE() {
+		return STATE;
+	}
+	public void setSTATE(Integer sTATE) {
+		STATE = sTATE;
+	}
+	public Integer getORDERLINE() {
+		return ORDERLINE;
+	}
+	public void setORDERLINE(Integer oRDERLINE) {
+		ORDERLINE = oRDERLINE;
+	}
+	public Integer getREVIEW() {
+		return REVIEW;
+	}
+	public void setREVIEW(Integer rEVIEW) {
+		REVIEW = rEVIEW;
+	}
+	public Integer getCOMMENT_TIMES() {
+		return COMMENT_TIMES;
+	}
+	public void setCOMMENT_TIMES(Integer cOMMENT_TIMES) {
+		COMMENT_TIMES = cOMMENT_TIMES;
+	}
+	public Integer getHITS() {
+		return HITS;
+	}
+	public void setHITS(Integer hITS) {
+		HITS = hITS;
+	}
+	public Integer getIN_FLAG() {
+		return IN_FLAG;
+	}
+	public void setIN_FLAG(Integer iN_FLAG) {
+		IN_FLAG = iN_FLAG;
+	}
+	public String getUSER_RIGHT() {
+		return USER_RIGHT;
+	}
+	public void setUSER_RIGHT(String uSER_RIGHT) {
+		USER_RIGHT = uSER_RIGHT;
+	}
+}
